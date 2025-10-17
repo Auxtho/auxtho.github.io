0542f883f8c821c51ba90c9c2646e1342e486a69
AuxthoAdmin <auxtho.official@gmail.com>
2025-10-17 21:22:32 +0900

    build: fix Tailwind content paths and rebuild inline.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.html",
+  content: ["./*.html", "./assets/app.js"],
+  theme: { extend: {} },
+
+  safelist: [
+  
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
